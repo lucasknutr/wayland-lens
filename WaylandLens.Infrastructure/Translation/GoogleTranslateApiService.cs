7fa875ab5dc0463674356225f9b9eb17121548df
Lucas <lucasclaudinocc@gmail.com>
2026-08-24 16:32:53 -0300

    bugfix: fixing google translate api service not running properly

--- WaylandLens.Infrastructure/Translation/GoogleTranslateApiService.cs
+++ WaylandLens.Infrastructure/Translation/GoogleTranslateApiService.cs
@@ -7,15 +7,20 @@ public class GoogleTranslateApiService(HttpClient httpClient) : ITranslationServ
 {
     public async Task<string> TranslateAsync(string text, string targetLanguage)
     { 
+        if (string.IsNullOrWhiteSpace(text)) return string.Empty;
+        
         // Uri.EspapeDataString() method will "URL encode" our text
         string url = $"https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl={targetLanguage}&dt=t&q={Uri.EscapeDataString(text)}";
-
         var responseString = await httpClient.GetStringAsync(url);
-
         using var document = JsonDocument.Parse(responseString);
 
-        var translatedText = document.RootElement[0][0][0].GetString();
+        var sb = new System.Text.StringBuilder();
         
-        return translatedText;
+        foreach (var element in document.RootElement[0].EnumerateArray())
+        {
+            sb.Append(element[0].GetString());
+        }
+
+        return sb.ToString() ?? string.Empty;
     }
 }
